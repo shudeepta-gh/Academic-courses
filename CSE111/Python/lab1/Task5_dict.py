e70209682a65767684a7bb3c8a978d920c58d5a3
Shudeepta Roy Mou <shudeeptaroy25@gmail.com>
2025-06-12 04:30:25 +1000

    Update Task5_dict.py

--- CSE111/Python/lab1/Task5_dict.py
+++ CSE111/Python/lab1/Task5_dict.py
@@ -36,3 +36,39 @@ if freq_dict_1 == freq_dict_2:
   print('Those are anagrams')
 else:
   print('Those are not anagrams')
+
+#without using dict
+word_1=input()
+word_2=input()
+
+count=0
+str_1=' '
+for char in word_1:
+  if char == ' ':
+    continue
+  elif ord('A') <= ord(char) <= ord('Z'):
+    str_1 += chr(ord(char) + 32)
+  else:
+    str_1 += char
+
+
+str_2 = ''
+for char in word_2:
+  if char == ' ':
+    continue
+  elif ord('A') <= ord(char) <= ord('Z'):
+    str_2 += chr(ord(char) + 32)
+  else:
+    str_2 += char
+
+
+for i in range(len(str_1)):
+  for j in range(len(str_2)):
+    if(str_1[i]==str_2[j]):
+       count+=1
+
+if(count==len(word_1)):
+  print("anagrams")
+else:
+  print("not anagrams")
+
