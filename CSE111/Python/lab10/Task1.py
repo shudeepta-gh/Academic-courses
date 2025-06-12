54b473029f46811673a16ecf042d3c07f6cb5dc8
Shudeepta Roy Mou <shudeeptaroy25@gmail.com>
2025-06-13 00:27:50 +1000

    Create Task1.py

---
+++ CSE111/Python/lab10/Task1.py
@@ -0,0 +1,20 @@
+class Student:
+    def __init__(self, name='Just a student', dept='nothing'):
+        self.__name = name
+        self.__department = dept
+    def set_department(self, dept):
+        self.__department = dept
+    def get_name(self):
+        return self.__name
+    def set_name(self,name):
+        self.__name = name
+    def detail(self):
+        return 'Name: '+self.__name+' Department: '+self.__department
+
+class BBA_Student(Student):
+  def __init__(self,name="default",dept="BBA"):
+    super().__init__(name,dept)
+
+print(BBA_Student().detail())
+print(BBA_Student('Humpty Dumpty').detail())
+print(BBA_Student('Little Bo Peep').detail())
