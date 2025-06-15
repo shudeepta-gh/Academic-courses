97d9baeef6e03f2cd52fa441ab836541d7eb9fcd
Shudeepta Roy Mou <shudeeptaroy25@gmail.com>
2025-06-15 21:42:42 +1000

    Update and rename CSE110/Java/lab1.Java to CSE110/Java/lab1/Task12.java

---
+++ CSE110/Java/lab1/Task12.java
@@ -0,0 +1,13 @@
+public class Test
+{
+  public static void main(String [] args)
+  {
+    int radius = 4 ;
+    double circumference = (2*Math.PI*radius);
+    double area = (Math.PI*radius*radius);
+    System.out.println("The circumference of the circle is %.2f", circumference);
+    System.out.println("The circumferance of the circle is %.2f ", area);
+  }
+}
+
+
