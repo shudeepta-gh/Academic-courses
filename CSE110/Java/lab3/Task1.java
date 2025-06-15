c3812765fca1d2e315f1b2f16f292e9f8018d60e
Shudeepta Roy Mou <shudeeptaroy25@gmail.com>
2025-06-16 00:24:11 +1000

    Create Task1.java

---
+++ CSE110/Java/lab3/Task1.java
@@ -0,0 +1,17 @@
+public class Test
+{
+  public static void main(String [] args)
+  {
+   for(int count = 18;count<=63;count+=9)
+   {
+     if(count!= 63)
+     {
+       System.out.print(count+",");
+     }
+     else
+     {
+       System.out.println(count);
+     }
+   }
+  }
+}
