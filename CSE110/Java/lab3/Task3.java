01570d4466bdfb06daadb9a14fa162bed4a2d62b
Shudeepta Roy Mou <shudeeptaroy25@gmail.com>
2025-06-16 00:25:40 +1000

    Create Task3.java

---
+++ CSE110/Java/lab3/Task3.java
@@ -0,0 +1,15 @@
+public class Test
+{
+  public static void main(String [] args)
+  {
+   int number = 0;
+   for(int count = 0;count<=600;count+=1)
+   {
+    if(count%7==0 && count%9==0)
+    {
+      number+=count;
+    }
+   }
+   System.out.println(number);
+  }
+}
