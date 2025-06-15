b69fffd85d0f0569ff2f6e5bfafd7ec0cf097214
Shudeepta Roy Mou <shudeeptaroy25@gmail.com>
2025-06-15 23:11:01 +1000

    Create Task13.java

---
+++ CSE110/Java/lab2/Task13.java
@@ -0,0 +1,17 @@
+public class Test
+{
+  public static void main(String [] args)
+  {
+    for(int number = 24; number>= -6; number-=6)
+    {
+      if(number==-6)
+      {
+         System.out.println(number);
+      }
+      else
+      {
+      System.out.print( number + ",");
+      }
+    }
+  }
+}
