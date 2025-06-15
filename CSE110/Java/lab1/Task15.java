69bcc4a037eaa36dd0103d20d04b0a8bc3df562a
Shudeepta Roy Mou <shudeeptaroy25@gmail.com>
2025-06-15 21:46:19 +1000

    Update Task15.java

--- CSE110/Java/lab1/Task15.java
+++ CSE110/Java/lab1/Task15.java
@@ -19,3 +19,26 @@ public class Test
 
   }
 }
+
+//Without using third variable
+
+import java.util.Scanner;
+public class Test
+{
+  public static void main(String [] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter integer number 1:");
+    int a = sc.nextInt();
+    System.out.print("Enter integer number 2:");
+    int b = sc.nextInt();
+    System.out.println("Before swap: a="+ a + " b="+b);
+
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+    System.out.println("After swap: a="+ a + " b="+b);
+
+  }
+}
