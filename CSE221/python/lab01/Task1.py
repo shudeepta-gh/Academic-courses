503b1045eff5a38f46473e6be7f6535d3657fc4c
Shudeepta Roy Mou <shudeeptaroy25@gmail.com>
2025-09-23 02:33:01 +1000

    Create Task1.py

---
+++ CSE221/python/lab01/Task1.py
@@ -0,0 +1,13 @@
+#Task A
+
+T = int(input())
+
+for i in range(T):
+    N = int(input())
+
+
+    if(N%2 == 0):
+        print(f"{N} is an Even number.")
+
+    else:
+        print(f"{N} is an Odd number.")
